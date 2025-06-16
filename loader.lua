6661cdde7167c300588edbe29f18a93eafb2f6d0
Ninja974 <155409865+Ninja974@users.noreply.github.com>
2025-06-15 22:39:36 -0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,13 +1,16 @@
---Loader
+-- Loader
 local executor = identifyexecutor()
+local client = game.Players.LocalPlayer
+local TeleportService = game:GetService("TeleportService")
 local GuiService = game:GetService("GuiService")
 local HttpService = game:GetService("HttpService")
+
 GuiService.ErrorMessageChanged:Connect(function()
-	TeleportService:Teleport(5956785391, client)
+    TeleportService:Teleport(5956785391, client)
 end)
 
 local fixeable = {
-    "Solara";
+    "Solara",
     "Xeno"
 }
 
@@ -19,33 +22,40 @@ if table.find(fixeable, executor) then
         Content = `Enabling {executor} Support (Script Might Take Longer Than Usual To Load)`,
         Duration = 5
     })
-    getgenv().require = function(obj:LocalScript|ModuleScript)
+    getgenv().require = function(obj: LocalScript | ModuleScript)
         local succ, ret = pcall(function()
             return loadstring(decompile(obj))()
         end)
         if succ then
             return ret
         else
-            return loadstring(game:HttpGet(string.gsub(`{modules}{obj:GetFullName()}.lua`, " ", "%%20")))()
+            local url = string.gsub(`{modules}{obj:GetFullName()}.lua`, " ", "%%20")
+            return loadstring(game:HttpGet(url))()
         end
     end
 end
 
-local baseUrl = `https://raw.githubusercontent.com/Ninja974/scripts/refs/heads/main/{game.GameId}/{game.PlaceId}.lua`
-local base64url = `https://api.github.com/repos/Ninja974/scripts/contents/{game.GameId}/{game.PlaceId}.lua?ref=main`
+local baseUrl = "https://raw.githubusercontent.com/Ninja974/scripts/refs/heads/main/" .. game.PlaceId .. ".lua"
+local base64url = "https://api.github.com/repos/Ninja974/scripts/contents/" .. game.PlaceId .. ".lua?ref=main"
 
 if base64 and base64.decode then
-    local response = game:HttpGet(base64url)
-    local data = HttpService:JSONDecode(response)
-
-    local base64decoded = base64.decode(data.content:gsub("\n", ""))
-    loadstring(base64decoded)()
+    local succ, err = pcall(function()
+        local response = game:HttpGet(base64url)
+        local data = HttpService:JSONDecode(response)
+        local base64decoded = base64.decode(data.content:gsub("\n", ""))
+        loadstring(base64decoded)()
+    end)
+    if not succ then
+        print(err)
+        pcall(function()
+            loadstring(game:HttpGet(baseUrl))()
+        end)
+    end
 else
-	local succ, err = pcall(function()
-		loadstring(game:HttpGet(baseurl))()
-	end)
-	
-	if not succ then
-		print(err)
-	end
+    local succ, err = pcall(function()
+        loadstring(game:HttpGet(baseUrl))()
+    end)
+    if not succ then
+        print(err)
+    end
 end
